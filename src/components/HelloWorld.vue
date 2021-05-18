f774cb0fdf77de6d8148f0fd20aa68f7a3ccf712
fandipres <fandi.simamora@gmail.com>
2021-05-18 23:37:27 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>Selamat Datang</div>
+    <ul>
+      <li v-for="item in todos">{{item.desc}}</li>
+    </ul>
+    <input v-model="myText"/>
+    <button @click="addTodos">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      todos: [
+        {desc: 'Makan Durian'},
+        {desc: 'Makan Tempe'},
+        {desc: 'Coding JS'}
+      ],
+      myText: ""
+    }
+  },
+  methods:{
+    addTodos: function(){
+      this.todos.push({desc: this.myText})    
+    }
+  }
+}
+</script>
