01be644e64df5343ee2a94f1470adc2a55024582
fandipres <fandi.simamora@gmail.com>
2021-06-13 15:18:37 +0700

    menambahkan frontend user dan pesan error

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <h1>Berikut adalah daftar tugas kita</h1>
+    <h1>Berikut adalah semua pengguna kita</h1>
     <ul>
-      <li v-for="item in todos" :key="item.id">{{item.deskripsi}} <button @click="hapus(item.id)">x</button></li>
+      <li v-for="item in users" :key="item.id">{{item.username}} <button @click="hapus(item.id)">x</button></li>
     </ul>
-    <input v-model="username"/>
-    <input v-model="password"/>
+    <input v-model="username" placeholder="username"/>
+    &nbsp
+    <input v-model="password" placeholder="password"/>
     <button @click="tambah">Add</button>
   </div>
 </template>
@@ -22,25 +23,51 @@ export default {
     }
   },
   created: function(){
-    axios.get('http://localhost:3000/user')
+    const username = localStorage.getItem('usr')
+    const password = localStorage.getItem('pwd')
+    axios.get('http://localhost:3000/user', {headers: {username, password}})
     .then((response) => {
-      this.todos = response.data
+      this.users = response.data
+    })
+    .catch((error) =>{
+      if(error.response.status === 401){
+        alert("Please login to see this page.")
+      }
     })
   },
   methods:{
     tambah: function(){
-      const newItem = {deskripsi: this.myText}
-      axios.post('http://localhost:3000/user', newItem)
-      .then(() => {
-        this.todos.push(newItem)
-        window.location.reload()
+      const username = localStorage.getItem('usr')
+      const password = localStorage.getItem('pwd')
+      const newItem = {username: this.username, password: this.password}
+      axios.post('http://localhost:3000/user', newItem, {headers: {username, password}})
+      .then((response) => {
+        this.users.push(response.data)
+      })
+      .catch((error) => {
+          if(error.response.status === 401){
+            alert("Please login to do this action.")
+          }
+          if(error.response.status === 405){
+            alert("This username is already taken.")
+          }
       })
     },
     hapus: function(id){
-      var index = this.todos.findIndex(obj => obj.id === id)
-      axios.delete(`http://localhost:3000/user/${id}`)
-      .then(() => {
-        this.todos.splice(index, 1)
+      const username = localStorage.getItem('usr')
+      const password = localStorage.getItem('pwd')
+      var index = this.users.findIndex(obj => obj.id === id)
+      axios.delete(`http://localhost:3000/user/${id}`, {headers: {username, password}})
+      .then((response) => {
+        this.users.splice(index, 1)
+      })
+      .catch((error) => {
+        if(error.response.status === 401){
+          alert("Please login to see this page.")
+        }
+        if(error.response.status === 405){
+          alert("Can't delete, only 1 user in database.")
+        }
       })
     }
   }
