fde55ad83ce2d282bc769e81f232b61077872692
fandipres <fandi.simamora@gmail.com>
2021-06-13 13:10:43 +0700

    menambahkan header pada router todo

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -20,23 +20,28 @@ export default {
     }
   },
   created: function(){
-    axios.get('http://localhost:3000/todo')
+    const username = localStorage.getItem('usr')
+    const password = localStorage.getItem('pwd')
+    axios.get('http://localhost:3000/todo', {headers: {username, password}})
     .then((response) => {
       this.todos = response.data
     })
   },
   methods:{
     tambah: function(){
+      const username = localStorage.getItem('usr')
+      const password = localStorage.getItem('pwd')
       const newItem = {deskripsi: this.myText}
-      axios.post('http://localhost:3000/todo', newItem)
-      .then(() => {
-        this.todos.push(newItem)
-        window.location.reload()
+      axios.post('http://localhost:3000/todo', newItem, {headers: {username, password}})
+      .then((response) => {
+        this.todos.push(response.data)
       })
     },
     hapus: function(id){
+      const username = localStorage.getItem('usr')
+      const password = localStorage.getItem('pwd')
       var index = this.todos.findIndex(obj => obj.id === id)
-      axios.delete(`http://localhost:3000/todo/${id}`)
+      axios.delete(`http://localhost:3000/todo/${id}`, {headers: {username, password}})
       .then(() => {
         this.todos.splice(index, 1)
       })
