b45ec8d3cf79e8c058b5eaff6021e94a06a8ef10
fandipres <fandi.simamora@gmail.com>
2021-06-13 13:10:01 +0700

    menambahkan router pada frontend

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <Todo/>
-  <User/>
+<router-link to="/">Halaman Auth</router-link>
+&nbsp
+<router-link to="/todo">Halaman Todo</router-link>
+&nbsp
+<router-link to="/user">Halaman User</router-link>
+<router-view></router-view>
 </template>
 
 <script setup>
